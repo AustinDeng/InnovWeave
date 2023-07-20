0556954917b485aa1bf2c65f6719c96d085946ab
Rollingegg <1244797125@qq.com>
2023-07-20 17:03:22 +0800

    mod: 按钮恢复原位

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -134,6 +134,18 @@ export function SideBar(props: { className?: string }) {
           <ChatGptIcon width="40px" height="40px" />
         </div>
       </div>
+
+      <div
+        className={styles["sidebar-body"]}
+        onClick={(e) => {
+          if (e.target === e.currentTarget) {
+            navigate(Path.Home);
+          }
+        }}
+      >
+        <ChatList narrow={shouldNarrow} />
+      </div>
+
       <div className={styles["sidebar-tail"]}>
         <div className={styles["sidebar-actions"]}>
           <div className={styles["sidebar-action"] + " " + styles.mobile}>
@@ -172,16 +184,6 @@ export function SideBar(props: { className?: string }) {
             shadow
           />
         </div>
-      </div>
-      <div
-        className={styles["sidebar-body"]}
-        onClick={(e) => {
-          if (e.target === e.currentTarget) {
-            navigate(Path.Home);
-          }
-        }}
-      >
-        <ChatList narrow={shouldNarrow} />
       </div>
     </div>
   );
